7b55b874eb4b526752e85c34762f78eb06f7389d
Erik Abdala <144078376+AbdalaErik@users.noreply.github.com>
2024-11-26 17:09:45 -0300

    commit for testing 9

--- tests/Feature/StudentTest.php
+++ tests/Feature/StudentTest.php
@@ -15,7 +15,7 @@ class StudentTest extends TestCase
      */
     public function test_example(): void
     {
-        $response = $this->get('/');
+        $response = $this->get('/'); //
 
         $response->assertStatus(200);
     }
